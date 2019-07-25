fa92e00905daece9078950823299aead2098ce67
fanhexin <fanhexin@gmail.com>
2019-07-25 16:25:39 +0800

    首次提交

---
+++ Localization.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasyLocalization
+{
+    public abstract class Localization : ScriptableObject
+    {
+        public static Localization instance { get; private set; }
+
+        public event Action ZoneChangeEv;
+    
+        public string this[string key]
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(key))
+                {
+                    return string.Empty;
+                }
+                
+                _dic.TryGetValue(key, out string value);
+                return value;
+            }
+        }
+
+        [NonSerialized]
+        IReadOnlyDictionary<string, string> _dic;
+
+        protected Localization()
+        {
+            if (instance != null)
+            {
+                throw new Exception($"{name} need to be singleton!");
+            }
+            instance = this;
+        }
+
+        protected virtual void OnEnable()
+        {
+            _dic = LoadDic();
+        }
+
+        protected abstract IReadOnlyDictionary<string, string> LoadDic();
+
+        public void UpdateDic(IReadOnlyDictionary<string, string> dic)
+        {
+            if (_dic == dic)
+            {
+                return;
+            }
+            
+            _dic = dic;
+            ZoneChangeEv?.Invoke();
+        }
+        
+#if UNITY_EDITOR
+        public IEnumerable<string> keys => _dic.Keys;
+        public void Reload()
+        {
+            UpdateDic(LoadDic());
+        }
+#endif
+    }
+}
---
+++ LocalizationText.cs
@@ -0,0 +1,78 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EasyLocalization
+{
+    [AddComponentMenu("UI/LocalizationText", 11)]
+    public class LocalizationText : Text
+    {
+        [SerializeField] string _localizationKey;
+        [SerializeField] Localization _localization;
+
+        public string localizationKey
+        {
+            set
+            {
+                _localizationKey = value;
+                UpdateText();
+            }
+        }
+
+        object[] _formatArgs;
+
+        #if UNITY_EDITOR
+        protected override void Reset()
+        {
+            base.Reset();
+            if (_localization != null)
+            {
+                return;
+            }
+
+            string[] ret = AssetDatabase.FindAssets($"t: {nameof(Localization)}");
+            if (ret == null || ret.Length == 0)
+            {
+                Debug.LogError($"{nameof(Localization)}'s implementation not found!");
+                return;
+            }
+
+            _localization = AssetDatabase.LoadAssetAtPath<Localization>(AssetDatabase.GUIDToAssetPath(ret[0]));
+        }
+        #endif
+
+        protected override void Start()
+        {
+            base.Start();
+            UpdateText();
+            _localization.ZoneChangeEv += UpdateText;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            _localization.ZoneChangeEv -= UpdateText;
+        }
+
+        void UpdateText()
+        {
+            string value = _localization[_localizationKey];
+            if (!string.IsNullOrEmpty(value))
+            {
+                text = _formatArgs == null ? value : string.Format(value, _formatArgs);
+            }
+        }
+
+        public void Format(params object[] args)
+        {
+            FormatByKey(_localizationKey, args);
+        }
+
+        public void FormatByKey(string localizationKey, params object[] args)
+        {
+            string value = _localization[localizationKey];
+            text = string.Format(string.IsNullOrEmpty(value)?text:value, args);
+            _formatArgs = args;
+        }
+    }
+}
